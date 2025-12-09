f7c37b5ca8602cfd99371f5aa48ca278d44e0200
kenn1d <makarkibanov20@gmail.com>
2025-12-09 12:45:20 +0500

    Созданный классы для приложения.Гашев

---
+++ EventManagerTelegramBot/Classes/Events.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagerTelegramBot.Classes
+{
+    public class Events
+    {
+        public DateTime Time { get; set; }
+        public string Message { get; set; }
+        public Events(DateTime time, string message)
+        {
+            Time = time;
+            Message = message;
+        }
+    }
+}
---
+++ EventManagerTelegramBot/Classes/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagerTelegramBot.Classes
+{
+    public class Users
+    {
+        public long IdUser { get; set; }
+        public List<Events> Events { get; set; }
+        public Users(long idUser) 
+        {
+            IdUser = idUser;
+            Events = new List<Events>();
+        }
+    }
+}
