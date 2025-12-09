70062ff0ab64872c4449c7548ee97e6911c5c3bb
kenn1d <makarkibanov20@gmail.com>
2025-12-09 12:57:05 +0500

    Разработанный методы создания кнопок, и проверка на вводимую дату и время

--- EventManagerTelegramBot/Worker.cs
+++ EventManagerTelegramBot/Worker.cs
@@ -1,9 +1,35 @@
+using EventManagerTelegramBot.Classes;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
 namespace EventManagerTelegramBot
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
-
+        readonly string Token = "полученный телеграмм токен";
+        TelegramBotClient TelegramBotClient;
+        List<Users> Users = new List<Users>();
+        Timer Timer;
+        List<string> Messages = new List<string>()
+        {
+            "Здравствуйте!"
+            +$"\nРады приветствовать вас в Telegram-боте @Напоминатор@!"
+            +"\nНаш бот создан для того, чтобы напоминать вам о важных событиях и мероприятиях. С ним вы точно не пропустите ничего важного!"
+            +"\nНе забудьте добавить бота в список своих контактов и настроить уведомления. Тогда вы всегда будете в курсе событий!",
+            "Укажите дату и время напоминания в следующем формате: "
+            +"\n<i><b>21:51 26.04.2025</b>"
+            +"\n Напомни о том что я хотел сходить в магазин.</i>",
+            "Кажется что-то не получилось."
+            +"\n<i><b>21:51 26.04.2025</b>"
+            +"\n Напомни о том что я хотел сходить в магазин.</i>",
+            "",
+            "Задачи пользователя не найдены.",
+            "Событие удалено.",
+            "Все события удалены."
+        };
+        
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
@@ -20,5 +46,38 @@ namespace EventManagerTelegramBot
                 await Task.Delay(1000, stoppingToken);
             }
         }
+        /// <summary>
+        /// Проверка корректности ввода даты и времени
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="time"></param>
+        /// <returns>Результат преобразования значения в дату и время </returns>
+        public bool CheckFormatDateTime(string value, out DateTime time)
+        {
+            return DateTime.TryParse(value, out time);
+        }
+        private static ReplyKeyboardMarkup GetButtons()
+        {
+            List<KeyboardButton> keyboardButtons = new List<KeyboardButton>();
+            keyboardButtons.Add(new KeyboardButton("Удалить все задачи"));
+            return new ReplyKeyboardMarkup
+            {
+                Keyboard = new List<List<KeyboardButton>>
+                {
+                    keyboardButtons
+                }
+            };
+        }
+        ///<summary>
+        ///Создание кнопки для удаления конкретного события
+        /// </summary>
+        /// <returns>Список кнопок</returns>
+        public static InlineKeyboardMarkup DeleteEvent(string Message)
+        {
+            List<InlineKeyboardButton> inlineKeyboards = new List<InlineKeyboardButton>();
+            inlineKeyboards.Add(new InlineKeyboardButton("Удалить", Message));
+            return new InlineKeyboardMarkup(inlineKeyboards);
+        }
+
     }
 }
