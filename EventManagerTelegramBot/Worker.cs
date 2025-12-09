fce83cbedcd43d2772bc74ce2b1bc114e75c2d03
kenn1d <makarkibanov20@gmail.com>
2025-12-09 13:33:33 +0500

    Разработанные методы получения обновлений и ошибок.

--- EventManagerTelegramBot/Worker.cs
+++ EventManagerTelegramBot/Worker.cs
@@ -1,5 +1,6 @@
 using EventManagerTelegramBot.Classes;
 using Telegram.Bot;
+using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
@@ -180,7 +181,32 @@ namespace EventManagerTelegramBot
                     ));
             }
         }
-
+        private async Task HandleUpdateAsync
+            (
+                ITelegramBotClient client,
+                Update update,
+                CancellationToken cancellationToken
+            )
+        {
+            if (update.Type == UpdateType.Message)
+                GetMessages(update.Message);
+            else if (update.Type == UpdateType.CallbackQuery)
+            {
+                CallbackQuery query = update.CallbackQuery;
+                Users User = Users.Find(x => x.IdUser == query.Message.Chat.Id);
+                Events Event = User.Events.Find(x => x.Message == query.Data);
+                User.Events.Remove(Event);
+                SendMessage(query.Message.Chat.Id, 5);
+            }
+        }
+        private async Task HandleErrorAsync(
+            ITelegramBotClient client,
+            Exception exception,
+            HandleErrorSource source,
+            CancellationToken token)
+        {
+            Console.WriteLine("Ошибка" + exception.Message);
+        }
 
     }
 }
